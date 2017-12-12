3b9f3dea92d0f392f4fda7f96bd68a30baed7d90
Mark Amabeli <markamabeli@marks-mac-mini.attlocal.net>
2017-12-11 19:30:41 -0500

    Initial commit. Will convert English to Morse dot notation. TODO: add handling for tabs and new paragraphs

---
+++ MCDoodle.API/MorseTranslator.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MCDoodle.API
+{
+    public static class MorseTranslator
+    {
+        public static string TranslateEnglishToMorse(List<string> input)
+        {
+            string result = string.Empty;
+            // A good tool to test intended output is https://morsecode.scphillips.com/translator.html.
+            // The result will be one large sentence, with additional space to designate that there is a paragraph.
+            for(int i=0; i<input.Count; i++)
+            {
+                string line = input[i];
+                TranslateEnglishLettersToMorse(ref line);
+                result += line;
+            }
+
+            return result;
+        }
+
+        private static void TranslateEnglishLettersToMorse(ref string lineInMorse)
+        {
+            lineInMorse = lineInMorse.ToLower();
+            Regex rgx = new Regex("[^a-z0-9 -]");
+            lineInMorse = rgx.Replace(lineInMorse, "");
+            // Replace each letter with its Morse equivalent
+            lineInMorse = lineInMorse.Replace("a",".-");
+            lineInMorse = lineInMorse.Replace("b","-...");
+            lineInMorse = lineInMorse.Replace("c","-.-.");
+            lineInMorse = lineInMorse.Replace("d","-..");
+            lineInMorse = lineInMorse.Replace("e",".");
+            lineInMorse = lineInMorse.Replace("f","..-.");
+            lineInMorse = lineInMorse.Replace("g","--.");
+            lineInMorse = lineInMorse.Replace("h","....");
+            lineInMorse = lineInMorse.Replace("i","..");
+            lineInMorse = lineInMorse.Replace("j",".---");
+            lineInMorse = lineInMorse.Replace("k","-.-");
+            lineInMorse = lineInMorse.Replace("l",".-..");
+            lineInMorse = lineInMorse.Replace("m","--");
+            lineInMorse = lineInMorse.Replace("n","-.");
+            lineInMorse = lineInMorse.Replace("o","---");
+            lineInMorse = lineInMorse.Replace("p",".--.");
+            lineInMorse = lineInMorse.Replace("q","--.-");
+            lineInMorse = lineInMorse.Replace("r",".-.");
+            lineInMorse = lineInMorse.Replace("s","...");
+            lineInMorse = lineInMorse.Replace("t","-");
+            lineInMorse = lineInMorse.Replace("u","..-");
+            lineInMorse = lineInMorse.Replace("v","...-");
+            lineInMorse = lineInMorse.Replace("w",".--");
+            lineInMorse = lineInMorse.Replace("x","-..-");
+            lineInMorse = lineInMorse.Replace("y","-.--");
+            lineInMorse = lineInMorse.Replace("z","--..");
+            lineInMorse = lineInMorse.Replace("0","-----");
+            lineInMorse = lineInMorse.Replace("1",".----");
+            lineInMorse = lineInMorse.Replace("2","..---");
+            lineInMorse = lineInMorse.Replace("3","...--");
+            lineInMorse = lineInMorse.Replace("4","....-");
+            lineInMorse = lineInMorse.Replace("5",".....");
+            lineInMorse = lineInMorse.Replace("6","-....");
+            lineInMorse = lineInMorse.Replace("7","--...");
+            lineInMorse = lineInMorse.Replace("8","---..");
+            lineInMorse = lineInMorse.Replace("9","----.");
+            lineInMorse = lineInMorse.Replace(" "," | ");
+
+            // return lineInMorse;
+        }
+    }
+}
---
+++ MCDoodle.API/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MCDoodle.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> morseInput = new List<string>();
+            using(StreamReader reader = new StreamReader("../MCDoodleData/InputData.txt"))
+            {
+                while(!reader.EndOfStream)
+                {
+                    morseInput.Add(reader.ReadLine());
+                }
+            }
+
+            string inputInMorse = MorseTranslator.TranslateEnglishToMorse(morseInput);
+            Console.WriteLine(inputInMorse);
+        }
+    }
+}
