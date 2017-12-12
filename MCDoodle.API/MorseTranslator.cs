bf395d88f754ae6fd1a4a4decddbda396bead6d9
Mark Amabeli <markamabeli@marks-mac-mini.attlocal.net>
2017-12-11 21:06:10 -0500

    MorseTranslator.cs: trimmed the input before translating, replaced the spaces first to avoid additional characters after replacing the letters, removed blank rows before translating, replaced all duplicate spaces with a single space, replaced punctuation with ||, fixerd issues relating to single spaces after punctuation.

--- MCDoodle.API/MorseTranslator.cs
+++ MCDoodle.API/MorseTranslator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace MCDoodle.API
@@ -7,6 +8,16 @@ namespace MCDoodle.API
     {
         public static string TranslateEnglishToMorse(List<string> input)
         {
+            // remove blank rows
+            for(int i=0; i<input.Count; i++)
+            {
+                if(input[i] == "")
+                {
+                    input.RemoveAt(i);
+                    i--;
+                }
+            }
+
             string result = string.Empty;
             // A good tool to test intended output is https://morsecode.scphillips.com/translator.html.
             // The result will be one large sentence, with additional space to designate that there is a paragraph.
@@ -22,47 +33,57 @@ namespace MCDoodle.API
 
         private static void TranslateEnglishLettersToMorse(ref string lineInMorse)
         {
-            lineInMorse = lineInMorse.ToLower();
-            Regex rgx = new Regex("[^a-z0-9 -]");
-            lineInMorse = rgx.Replace(lineInMorse, "");
-            // Replace each letter with its Morse equivalent
-            lineInMorse = lineInMorse.Replace("a",".-");
-            lineInMorse = lineInMorse.Replace("b","-...");
-            lineInMorse = lineInMorse.Replace("c","-.-.");
-            lineInMorse = lineInMorse.Replace("d","-..");
-            lineInMorse = lineInMorse.Replace("e",".");
-            lineInMorse = lineInMorse.Replace("f","..-.");
-            lineInMorse = lineInMorse.Replace("g","--.");
-            lineInMorse = lineInMorse.Replace("h","....");
-            lineInMorse = lineInMorse.Replace("i","..");
-            lineInMorse = lineInMorse.Replace("j",".---");
-            lineInMorse = lineInMorse.Replace("k","-.-");
-            lineInMorse = lineInMorse.Replace("l",".-..");
-            lineInMorse = lineInMorse.Replace("m","--");
-            lineInMorse = lineInMorse.Replace("n","-.");
-            lineInMorse = lineInMorse.Replace("o","---");
-            lineInMorse = lineInMorse.Replace("p",".--.");
-            lineInMorse = lineInMorse.Replace("q","--.-");
-            lineInMorse = lineInMorse.Replace("r",".-.");
-            lineInMorse = lineInMorse.Replace("s","...");
-            lineInMorse = lineInMorse.Replace("t","-");
-            lineInMorse = lineInMorse.Replace("u","..-");
-            lineInMorse = lineInMorse.Replace("v","...-");
-            lineInMorse = lineInMorse.Replace("w",".--");
-            lineInMorse = lineInMorse.Replace("x","-..-");
-            lineInMorse = lineInMorse.Replace("y","-.--");
-            lineInMorse = lineInMorse.Replace("z","--..");
-            lineInMorse = lineInMorse.Replace("0","-----");
-            lineInMorse = lineInMorse.Replace("1",".----");
-            lineInMorse = lineInMorse.Replace("2","..---");
-            lineInMorse = lineInMorse.Replace("3","...--");
-            lineInMorse = lineInMorse.Replace("4","....-");
-            lineInMorse = lineInMorse.Replace("5",".....");
-            lineInMorse = lineInMorse.Replace("6","-....");
-            lineInMorse = lineInMorse.Replace("7","--...");
-            lineInMorse = lineInMorse.Replace("8","---..");
-            lineInMorse = lineInMorse.Replace("9","----.");
-            lineInMorse = lineInMorse.Replace(" "," | ");
+            lineInMorse = lineInMorse.ToLower().Trim();
+
+            // remove all non-alphanumeric characters (except periods)
+            Regex rgx_nonAlphaNumeric = new Regex(@"[^a-z0-9\.\?\! -]");
+
+            // replace all duplicate spaces with a single space
+            lineInMorse = rgx_nonAlphaNumeric.Replace(lineInMorse, "");
+            Regex rgx_duplicateSpaces = new Regex(@"[ ]{1,}");
+
+            lineInMorse = rgx_duplicateSpaces.Replace(lineInMorse," ");
+            // Replace each letter with its Morse e quivalent
+            lineInMorse = lineInMorse.Replace(" ","| ");
+            lineInMorse = lineInMorse.Replace("?"," ||");
+            lineInMorse = lineInMorse.Replace("!"," ||");
+            lineInMorse = lineInMorse.Replace(".","  ||");
+            lineInMorse = lineInMorse.Replace("a",".- ");
+            lineInMorse = lineInMorse.Replace("b","-... ");
+            lineInMorse = lineInMorse.Replace("c","-.-. ");
+            lineInMorse = lineInMorse.Replace("d","-.. ");
+            lineInMorse = lineInMorse.Replace("e",". ");
+            lineInMorse = lineInMorse.Replace("f","..-. ");
+            lineInMorse = lineInMorse.Replace("g","--. ");
+            lineInMorse = lineInMorse.Replace("h",".... ");
+            lineInMorse = lineInMorse.Replace("i",".. ");
+            lineInMorse = lineInMorse.Replace("j",".--- ");
+            lineInMorse = lineInMorse.Replace("k","-.- ");
+            lineInMorse = lineInMorse.Replace("l",".-.. ");
+            lineInMorse = lineInMorse.Replace("m","-- ");
+            lineInMorse = lineInMorse.Replace("n","-. ");
+            lineInMorse = lineInMorse.Replace("o","--- ");
+            lineInMorse = lineInMorse.Replace("p",".--. ");
+            lineInMorse = lineInMorse.Replace("q","--.- ");
+            lineInMorse = lineInMorse.Replace("r",".-. ");
+            lineInMorse = lineInMorse.Replace("s","... ");
+            lineInMorse = lineInMorse.Replace("t","- ");
+            lineInMorse = lineInMorse.Replace("u","..- ");
+            lineInMorse = lineInMorse.Replace("v","...- ");
+            lineInMorse = lineInMorse.Replace("w",".-- ");
+            lineInMorse = lineInMorse.Replace("x","-..- ");
+            lineInMorse = lineInMorse.Replace("y","-.-- ");
+            lineInMorse = lineInMorse.Replace("z","--.. ");
+            lineInMorse = lineInMorse.Replace("0","----- ");
+            lineInMorse = lineInMorse.Replace("1",".---- ");
+            lineInMorse = lineInMorse.Replace("2","..--- ");
+            lineInMorse = lineInMorse.Replace("3","...-- ");
+            lineInMorse = lineInMorse.Replace("4","....- ");
+            lineInMorse = lineInMorse.Replace("5","..... ");
+            lineInMorse = lineInMorse.Replace("6","-.... ");
+            lineInMorse = lineInMorse.Replace("7","--... ");
+            lineInMorse = lineInMorse.Replace("8","---.. ");
+            lineInMorse = lineInMorse.Replace("9","----. ");
 
             // return lineInMorse;
         }
